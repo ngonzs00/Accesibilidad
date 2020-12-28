51587b24b80d4c391972177c7ac9442da98468ef
ngonzs00 <ngonzs00@estudiantes.unileon.es>
2020-12-28 21:44:21 +0100

    merchandising hecho un poco mas o menos

--- accesibilidad/src/views/Merchandising.vue
+++ accesibilidad/src/views/Merchandising.vue
@@ -1,100 +1,134 @@
-
 <template>
-<v-container fluid class="success" style="height:1500px">
+  <v-container class="warning" fluid grid-list-md>
+    <v-layout>
+      <v-app-bar app color="#191C1F">
+        <v-app-bar-nav-icon class="white--text"></v-app-bar-nav-icon> <!--Le añadimos un icono blanco-->
+        <v-toolbar-title class="white--text">Merchandaising</v-toolbar-title> <!--Le añadimos el titulo-->
+        <v-avatar size="40" class="ml-3">
+          <img src="../assets/LogoPanicoKernel.jpeg" absolute alt="">
+        </v-avatar>
 
-<v-col align="center">
-    <h1 class=" myfontGrey ml-12 ">Merchandising Pánico del Kernel</h1>
-  <v-row justify="center">
-    
-    <v-col
-      cols="12"
-      sm="6"
-      align="center"
-    >
-      <v-hover
-        v-slot:default="{ hover }"
-        close-delay="200"
-      >
-        <v-card
-          :elevation="hover ? 16 : 2"
-          class="mx-auto"
-          height="400"
-          max-width="435"
-          color="#1F2326"
-        >
-          <v-card-text class="font-weight-medium mt-12 text-center subtitle-1">
-            <img src="../assets/camiseta.png" absolute alt="">
-          </v-card-text>
+        <v-spacer></v-spacer>
+          <h1 class="white--text body-1 ma-5 hoverMouse" @click="goTo('')">Página principal</h1>
+          <h1 class="white--text body-1 ma-5 hoverMouse" @click="goTo('faq')">About us</h1>
+      </v-app-bar>
+    </v-layout>
+
+
+    <br />
+    <br />
+
+    <v-row>
+      <v-col cols="2"></v-col>
+      <v-col cols="4">
+        <v-card dark>
+          <v-col cols="1">
+            <img width="200" height="200" src="../assets/camiseta.png" />
+          </v-col>
+          <v-col cols="">
+            <v-list>
+              <p><strong>Precio: </strong> 15€</p>
+              <p><strong>Producto: </strong> Camiseta Panico del Kernel</p>
+              <p class="text-right"><v-btn class="ml-14" color="#1aff1a" text @click="snackbarDeContacto = false">Comprar</v-btn></p>
+            </v-list>
+          </v-col>
         </v-card>
-      </v-hover>
+      </v-col>
 
-        <h4 class="myfontGrey mt-5">Camiseta de hombre Pánico del kernel</h4><h4 class="myfontGreen">20€</h4>      
-
-    </v-col>
-
-    <v-col
-      cols="12"
-      sm="6"
-      align="center"
-    >
-      <v-hover
-        v-slot:default="{ hover }"
-        close-delay="200"
-      >
-        <v-card
-          :elevation="hover ? 16 : 2"
-          class="mx-auto"
-          height="400"
-          max-width="435"
-          color="#1F2326"
-        >
-          <v-card-text class="font-weight-medium mt-12 text-center subtitle-1">
-            <img src="../assets/camiseta.png" absolute alt="">
-          </v-card-text>
+      <v-col cols="4">
+        <v-card dark>
+          <v-col cols="1">
+            <img width="200" height="200" src="../assets/camiseta.png" />
+          </v-col>
+          <v-col cols="">
+            <v-list>
+              <p><strong>Precio: </strong>7€</p>
+              <p><strong>Producto: </strong> Camiseta Panico del Kernel</p>
+              <p class="text-right"><v-btn class="ml-14" color="#1aff1a" text @click="snackbarDeContacto = false">Comprar</v-btn></p>
+            </v-list>
+          </v-col>
         </v-card>
-      </v-hover>
+      </v-col>
+      <v-col cols="2"></v-col>
+    </v-row>
 
-        <h4 class="myfontGrey mt-5">La misma camiseta, pero muchísimo mejor</h4><h4 class="myfontGreen">2000€</h4>      
+    <br />
+    <br />
+    <v-row>
+      <v-col cols="2"></v-col>
+      <v-col cols="4">
+        <v-card dark>
+          <v-col cols="1">
+            <img width="200" height="200" src="../assets/camiseta.png" />
+          </v-col>
+          <v-col cols="">
+            <v-list>
+              <p><strong>Precio: </strong> 5€</p>
+              <p><strong>Producto: </strong> Camiseta Panico del Kernel Nueva</p>
+              <p class="text-right"><v-btn class="ml-14" color="#1aff1a" text @click="snackbarDeContacto = false">Comprar</v-btn></p>
+            </v-list>
+          </v-col>
+        </v-card>
+      </v-col>
 
+      <v-col cols="4">
+        <v-card dark>
+          <v-col cols="1">
+            <img width="200" height="200" src="../assets/camiseta.png" />
+          </v-col>
+          <v-col cols="">
+            <v-list>
+              <p><strong>Precio: </strong> 23€</p>
+              <p><strong>Producto: </strong> Camiseta Panico del Chupacabrar</p>
+              <p class="text-right"><v-btn class="ml-14" color="#1aff1a" text @click="snackbarDeContacto = false">Comprar</v-btn></p>
+            </v-list>
+          </v-col>
+        </v-card>
+      </v-col>
 
-    </v-col>
-  </v-row>
-</v-col>
+      <v-col cols="2"></v-col>
+    </v-row>
+
+    <br /><br /><br />
+    <br /><br /><br />
+    <br /><br /><br />
+    <br /><br /><br />
+    <br /><br /><br />
+    <br /><br /><br />
+    <br /><br /><br />
+    <br /><br /><br />
+
+    <!-- Footer de la aplicacion -->
+    <v-footer color="#191C1F" absolute class="font-weight-medium" style="height:100px">
+      <v-col class="text-center" cols="12">
+        <h1
+          class="overline pt-0 white--text"
+        >Copyright © 2020 DECLIVIA A.A.E LTD All rights reserved.</h1>
+        <h1 class="overline pt-3 white--text">
+          <a target="_blank" color="#8AC53E"></a> icons by
+          <a
+            target="_blank"
+            href="http://code.meta-platform.com/assets/mdi/preview.html"
+          >code.meta-platform</a>
+        </h1>
+      </v-col>
+    </v-footer>
   </v-container>
 </template>
 
 
-<style>
 
-  .myfontGrey{
 
-    color: #78838a;
-    font-family: 'Ubuntu Mono', monospace;
-
-  }
-
-  .myfontRed{
-
-    color: #f00a0a;
-    font-family: 'Ubuntu Mono', monospace;
-
-  }
-
-  .myfontWhite{
-
-    color: #ffffff;
-    font-family: 'Ubuntu Mono', monospace;
-
-  }
-
-  .myfontGreen{
-    
-    color: #8AC53E
-
-  }
-
-  .hoverMouse
-  {
-    cursor: pointer
-  }
-</style>
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  methods: {
+    goTo(dir) {
+      this.$router.push("/" + dir);
+    },
+  },
+};
+</script>
